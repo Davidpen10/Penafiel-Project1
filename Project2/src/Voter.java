5865988af013d3a39318c7399f21f07271d33c56
Davidpen10 <david.penafiel1@marist.edu>
2023-03-10 09:09:46 -0500

    method randomly selects a preferred candidate, marking the ballot for that candidate, and casting the ballot on the voting machine.

--- Project2/src/Voter.java
+++ Project2/src/Voter.java
@@ -12,3 +12,20 @@ public class Voter {
      */public void vote(VotingMachine machine) {
         // Get the ballot from the voting machine.
         Ballot ballot = machine.getBallot();
+
+        // Determine the number of candidates available on the ballot.
+        int numCandidates = ballot.getCandidates().length;
+
+
+        // Randomly select a preferred candidate to vote for.
+        int preferredCandidate = (int) (Math.random() * numCandidates);
+
+
+        // Mark the ballot for the preferred candidate.
+        ballot.mark(preferredCandidate);
+
+
+        // Cast the ballot on the voting machine.
+        machine.cast(ballot);
+    }
+}

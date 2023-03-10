f3508be7501faa5ec1227cb6b858ebd28d90eb89
Davidpen10 <david.penafiel1@marist.edu>
2023-03-10 09:29:57 -0500

    Input number of voters, candidates, times, candidate names. Close scanner.

--- Project2/src/VotingSimulation.java
+++ Project2/src/VotingSimulation.java
@@ -13,4 +13,17 @@ public class VotingSimulation {
      * Constructs a new VotingSimulation object, prompting the user for input and storing the values.
      */
 public class VotingSimulation {
-
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the amount of voters: ");
+    this.numVoters = in.nextInt();
+    System.out.println("Enter the amount of candidates: ");
+    this.numCandidates = in.nextInt();
+    System.out.println("Enter the amount of times to run the simulation: ");
+    this.numTimes = in.nextInt();
+    in.nextLine(); // consume the newline character after reading an int this.candidateNames = new String[this.numCandidates];
+        // for (int i = 0; i < this.numCandidates; i++) {
+            System.out.println("Enter the name of candidate " + (i + 1) + ": ");
+            this.candidateNames[i] = in.nextLine();
+        }
+       in.close();
+    }

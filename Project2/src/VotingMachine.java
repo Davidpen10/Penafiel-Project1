b3019a37e21ae653726c17d1a2c6f7c4b07af20b
Davidpen10 <david.penafiel1@marist.edu>
2023-03-10 09:11:56 -0500

    configured ballot template, an array to keep track of vote counts, and methods to configure the machine, cast ballots, and determine a winner.

--- Project2/src/VotingMachine.java
+++ Project2/src/VotingMachine.java
@@ -1,2 +1,17 @@
 public class VotingMachine {
 }
+    /**
+     * The VotingMachine class represents a voting machine that can be used to cast ballots for candidates.
+     * It contains a configuredBallot that serves as a template for creating ballots given to voters,
+     * a voteCounts array that keeps track of the number of votes for each candidate,
+     * and methods to cast ballots, configure the machine, and determine a winner.
+     */
+    private Ballot configuredBallot;
+    int[] voteCounts;
+
+
+/**
+ * Configures the voting machine to have the given number of candidates on the ballot.
+ *
+ * @param numCandidates the number of candidates on the ballot
+ */

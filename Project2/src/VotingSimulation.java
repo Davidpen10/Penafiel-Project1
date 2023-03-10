3323dbdd5935205b85fca6d94447b8a587c86847
Davidpen10 <david.penafiel1@marist.edu>
2023-03-10 09:34:49 -0500

    prompts user for input, runs voting simulation multiple times
    For each simulation round, a new VotingMachine and Voter objects are created.
    
    Voting results are calculated and printed.
    
    If a candidate receives >50% of the vote in the first round, they win.
    
    Otherwise, a runoff election is conducted and winner is declared.
    
    Winner of each round is printed to console.

--- Project2/src/VotingSimulation.java
+++ Project2/src/VotingSimulation.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  A class that simulates a voting system with a two-round election and a runoff election if needed.
  */
 public class VotingSimulation {
+
     private final int numVoters;
     private final int numCandidates;
     private final int numTimes;
@@ -12,42 +13,38 @@ public class VotingSimulation {
     /**
      * Constructs a new VotingSimulation object, prompting the user for input and storing the values.
      */
-public class VotingSimulation {
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter the amount of voters: ");
-    this.numVoters = in.nextInt();
-    System.out.println("Enter the amount of candidates: ");
-    this.numCandidates = in.nextInt();
-    System.out.println("Enter the amount of times to run the simulation: ");
-    this.numTimes = in.nextInt();
-    in.nextLine(); // consume the newline character after reading an int this.candidateNames = new String[this.numCandidates];
-        // for (int i = 0; i < this.numCandidates; i++) {
+    public VotingSimulation() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the amount of voters: ");
+        this.numVoters = in.nextInt();
+        System.out.println("Enter the amount of candidates: ");
+        this.numCandidates = in.nextInt();
+        System.out.println("Enter the amount of times to run the simulation: ");
+        this.numTimes = in.nextInt();
+        in.nextLine(); // consume the newline character after reading an int
+        this.candidateNames = new String[this.numCandidates];
+        for (int i = 0; i < this.numCandidates; i++) {
             System.out.println("Enter the name of candidate " + (i + 1) + ": ");
             this.candidateNames[i] = in.nextLine();
         }
-       in.close();
+        in.close();
     }
+
     public static void main(String[] args) {
         VotingSimulation simulation = new VotingSimulation();
         simulation.runSimulation();
     }
 /**
 
-
  Runs the simulation for a specified number of times, outputting the results of each round of voting.
-
 
  For each simulation round, creates a new VotingMachine object and a specified number of Voter objects.
 
-
  Calculates the voting results and prints them to the console.
-
 
  If a candidate receives more than 50% of the vote in the first round, declares them as the winner.
 
-
  Otherwise, conducts a runoff election and declares the winner of the runoff.
-
 
  Prints the winner of each simulation round to the console.
  */

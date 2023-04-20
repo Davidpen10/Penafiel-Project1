020329fbdcc4c7c17a621001a0bf6bac6fc869d2
Davidpen10 <david.penafiel1@marist.edu>
2023-04-20 13:40:27 -0400

    determine the number of available candidates in the ballot

--- Project3/src/LeadVoter.java
+++ Project3/src/LeadVoter.java
@@ -8,3 +8,27 @@ public class LeadVoter implements Voter {
 
         // Determine the number of candidates available on the ballot.
         int numCandidates = ballot.getCandidates().length;
+        //Get vote count for each candidate
+        int[] voteCounts = machine.getVoteCounts();
+
+        //Assume -1 is the highest vote count
+        int highest = -1;
+
+        //Index of the leading a candidate
+        int leadingCandidateIndex = -1;
+
+        for (int i = 0; i < voteCounts.length; i++){
+            if (voteCounts[i] > highest) {
+                highest = voteCounts[i];
+                leadingCandidateIndex = i;
+            }
+        }
+
+        // Mark the ballot for the preferred candidate.
+        ballot.mark(leadingCandidateIndex);
+
+
+        // Cast the ballot on the voting machine.
+        machine.cast(ballot);
+    }
+}

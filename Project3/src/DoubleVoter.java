c5b6189cff9372d8672a3cf5f044ace194e7779e
Davidpen10 <david.penafiel1@marist.edu>
2023-04-20 13:24:06 -0400

    created DoubleVoter.java to get the ballot from voting machine

---
+++ Project3/src/DoubleVoter.java
@@ -0,0 +1,10 @@
+public class DoubleVoter implements Voter {
+
+
+    @Override
+    public void vote(VotingMachine machine) {
+        // Get the ballot from the voting machine.
+        Ballot ballot = machine.getBallot();
+
+        // Determine the number of candidates available on the ballot.
+        int numCandidates = ballot.getCandidates().length;

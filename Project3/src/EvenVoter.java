d9a7c3d3220b6524d7e08caf6dc4a009d40cc662
Davidpen10 <david.penafiel1@marist.edu>
2023-04-20 13:35:52 -0400

    created public class EvenVoter.java to implement Voter

---
+++ Project3/src/EvenVoter.java
@@ -0,0 +1,26 @@
+public class EvenVoter implements Voter {
+    @Override
+    public void vote(VotingMachine machine) {
+        // Get the ballot from the voting machine.
+        Ballot ballot = machine.getBallot();
+
+        // Determine the number of candidates available on the ballot.
+        int numCandidates = ballot.getCandidates().length;
+
+
+        // Randomly select an even index candidate to vote for.
+        int preferredCandidate = 0;
+
+        do {
+            preferredCandidate = (int) (Math.random() * numCandidates);
+        } while (preferredCandidate % 2 != 1);
+
+
+        // Mark the ballot for the preferred candidate.
+        ballot.mark(preferredCandidate);
+
+
+        // Cast the ballot on the voting machine.
+        machine.cast(ballot);
+    }
+}

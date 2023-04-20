7867a2899ff07ae7da3399c51070fc2f7386ac6d
Davidpen10 <david.penafiel1@marist.edu>
2023-04-20 14:17:53 -0400

    runs, calculates and prints the winner of each simulation round

--- Project3/src/VotingSimulation.java
+++ Project3/src/VotingSimulation.java
@@ -49,3 +49,52 @@ public VotingSimulation() {
     }
     in.close();
 }
+    VotingSimulation simulation = new VotingSimulation();
+        simulation.runSimulation();
+}
+
+    /**
+     * Runs the simulation for a specified number of times, outputting the results of each round of voting.
+     * <p>
+     * For each simulation round, creates a new VotingMachine object and a specified number of Voter objects.
+     * <p>
+     * Calculates the voting results and prints them to the console.
+     * <p>
+     * If a candidate receives more than 50% of the vote in the first round, declares them as the winner.
+     * <p>
+     * Otherwise, conducts a runoff election and declares the winner of the runoff.
+     * <p>
+     * Prints the winner of each simulation round to the console.
+     */
+    public void runSimulation() {
+        for (int i = 0; i < this.numTimes; i++) {
+            VotingMachine votingMachine = new VotingMachine();
+            votingMachine.configure(this.numCandidates);
+
+            for (int j = 0; j < this.randomVoters; j++) {
+                Voter voter = new RandomVoter();
+                voter.vote(votingMachine);
+            }
+
+            for (int j = 0; j < this.oddVoters; j++) {
+                Voter voter = new OddVoter();
+                voter.vote(votingMachine);
+            }
+
+            for (int j = 0; j < this.evenVoters; j++) {
+                Voter voter = new EvenVoter();
+                voter.vote(votingMachine);
+            }
+
+            for (int j = 0; j < this.doubleVoters; j++) {
+                Voter voter = new DoubleVoter();
+                voter.vote(votingMachine);
+            }
+
+            for (int j = 0; j < this.loyalVoters; j++) {
+                Voter voter = new LoyalVoter(loyalVoterPrefferedCandidateIndex);
+                voter.vote(votingMachine);
+            }
+
+
+

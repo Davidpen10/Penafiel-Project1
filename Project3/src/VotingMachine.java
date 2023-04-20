ae83edf2d8494029c9fdfd579eb4858ed88c3021
Davidpen10 <david.penafiel1@marist.edu>
2023-04-20 14:01:00 -0400

    created public class VotingMachine.java

---
+++ Project3/src/VotingMachine.java
@@ -0,0 +1,22 @@
+public class VotingMachine {
+
+    /**
+     * The VotingMachine class represents a voting machine that can be used to cast ballots for candidates.
+     * It contains a configuredBallot that serves as a template for creating ballots given to voters,
+     * a voteCounts array that keeps track of the number of votes for each candidate,
+     * and methods to cast ballots, configure the machine, and determine a winner.
+     */
+    private Ballot configuredBallot;
+    int[] voteCounts;
+
+
+    /**
+     * Configures the voting machine to have the given number of candidates on the ballot.
+     *
+     * @param numCandidates the number of candidates on the ballot
+     */
+    public void configure(int numCandidates) {
+        configuredBallot = new Ballot(numCandidates);
+        voteCounts = new int[numCandidates];
+    }
+    

b682343cb29e8d7bba2e66831f17e2fa3bd3521c
Davidpen10 <david.penafiel1@marist.edu>
2023-04-20 13:37:58 -0400

    coded to give the vote to the candidate with the highest current votes

---
+++ Project3/src/LeadVoter.java
@@ -0,0 +1,10 @@
+//Gives the vote to the candidate with the highest current votes
+//Gives the vote to the first candidate if all candidates have 0 votes
+public class LeadVoter implements Voter {
+    @Override
+    public void vote(VotingMachine machine) {
+        // Get the ballot from the voting machine.
+        Ballot ballot = machine.getBallot();
+
+        // Determine the number of candidates available on the ballot.
+        int numCandidates = ballot.getCandidates().length;

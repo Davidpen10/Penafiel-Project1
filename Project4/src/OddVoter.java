93dcb2ab70747af33d0a5dd6854b41bacc37c394
Davidpen10 <david.penafiel1@marist.edu>
2023-05-06 10:08:39 -0400

    Mark the ballot for the preferred candidate

--- Project4/src/OddVoter.java
+++ Project4/src/OddVoter.java
@@ -6,3 +6,18 @@ public class OddVoter implements Voter {
 
         // Determine the number of candidates available on the ballot.
         int numCandidates = ballot.getCandidates().length;
+        // Randomly select an odd index candidate to vote for.
+        int preferredCandidate = 0;
+        do {
+            preferredCandidate = (int) (Math.random() * numCandidates);
+        } while (preferredCandidate % 2 != 1);
+
+
+        // Mark the ballot for the preferred candidate.
+        ballot.mark(preferredCandidate);
+
+
+        // Cast the ballot on the voting machine.
+        machine.cast(ballot);
+    }
+}

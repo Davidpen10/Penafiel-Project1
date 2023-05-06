c65feac604d60bbbf672b7ea03efcc4d552cbef5
Davidpen10 <david.penafiel1@marist.edu>
2023-05-06 10:10:35 -0400

    Configures the voting machine to have the given number of candidates on the ballot

---
+++ Project4/src/VotingMachine.java
@@ -0,0 +1,28 @@
+public class VotingMachine {
+
+    /**
+     * The VotingMachine class represents a voting machine that can be used to cast ballots for candidates.
+     * It contains a configuredBallot that serves as a template for creating ballots given to voters,
+     * a voteCounts array that keeps track of the number of votes for each candidate,
+     * and methods to cast ballots, configure the machine, and determine a winner.
+     */
+    private Ballot configuredBallot;
+    int[] voteCounts;
+
+
+    /**
+     * Configures the voting machine to have the given number of candidates on the ballot.
+     *
+     * @param numCandidates the number of candidates on the ballot
+     */
+    public void configure(int numCandidates) {
+        configuredBallot = new Ballot(numCandidates);
+        voteCounts = new int[numCandidates];
+    }
+    public int[] getVoteCounts() {
+        return voteCounts.clone();
+    }
+
+    public int[] getTopTwoCandidates() {
+        int[] voteCounts = this.getVoteCounts();
+        int[] topTwoCandidates = new int[2];

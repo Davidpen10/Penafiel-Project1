a2649c763054ecb02782712fdb26c61b5063ab2c
Davidpen10 <david.penafiel1@marist.edu>
2023-05-06 10:07:14 -0400

    created LoyalVoter

---
+++ Project4/src/LoyalVoter.java
@@ -0,0 +1,21 @@
+public class LoyalVoter implements Voter {
+    //The index of the candidate the voter is loyal to
+    private int candidateIndex;
+
+    //Constructor
+    public LoyalVoter(int candidateIndex) {
+        this.candidateIndex = candidateIndex;
+    }
+
+    @Override
+    public void vote(VotingMachine machine) {
+        // Get the ballot from the voting machine.
+        Ballot ballot = machine.getBallot();
+
+        // Mark the ballot for the loyal candidate.
+        ballot.mark(candidateIndex);
+
+        // Cast the ballot on the voting machine.
+        machine.cast(ballot);
+    }
+}

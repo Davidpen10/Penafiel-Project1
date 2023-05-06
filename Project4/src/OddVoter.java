d5b7779f28446043503b98ab465aa1845d914453
Davidpen10 <david.penafiel1@marist.edu>
2023-05-06 10:08:08 -0400

    Determine the number of candidates available on the ballot

---
+++ Project4/src/OddVoter.java
@@ -0,0 +1,8 @@
+public class OddVoter implements Voter {
+    @Override
+    public void vote(VotingMachine machine) {
+        // Get the ballot from the voting machine.
+        Ballot ballot = machine.getBallot();
+
+        // Determine the number of candidates available on the ballot.
+        int numCandidates = ballot.getCandidates().length;

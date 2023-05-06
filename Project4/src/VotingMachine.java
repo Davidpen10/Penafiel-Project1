c4ec9178093861a5a0f435b42bf35efc9306d013
Davidpen10 <david.penafiel1@marist.edu>
2023-05-06 10:11:13 -0400

    Find the two candidates with the highest vote counts

--- Project4/src/VotingMachine.java
+++ Project4/src/VotingMachine.java
@@ -26,3 +26,53 @@ public class VotingMachine {
     public int[] getTopTwoCandidates() {
         int[] voteCounts = this.getVoteCounts();
         int[] topTwoCandidates = new int[2];
+
+        // Initialize the top two candidates to the first two candidates
+        for (int i = 0; i < 2; i++) {
+            topTwoCandidates[i] = i;
+        }
+
+// Find the two candidates with the highest vote counts
+        for (int i = 2; i < voteCounts.length; i++) {
+            if (voteCounts[i] > voteCounts[topTwoCandidates[0]]) {
+                topTwoCandidates[1] = topTwoCandidates[0];
+                topTwoCandidates[0] = i;
+            } else if (voteCounts[i] > voteCounts[topTwoCandidates[1]]) {
+                topTwoCandidates[1] = i;
+            }
+        }
+        return topTwoCandidates;
+    }
+    /**
+     * Casts a ballot in the voting machine, incrementing the vote count for the preferred candidate
+     * if the ballot is valid (i.e. it contains a mark for only one candidate).
+     *
+     * @param ballot the ballot to cast
+     * @return true if the ballot was cast successfully, false otherwise
+     */
+    public void cast(Ballot ballot) {
+        boolean[] selectedCandidates = ballot.getCandidates();
+        int markedCandidate = -1;
+        for (int i = 0; i < selectedCandidates.length; i++) {
+            if (selectedCandidates[i]) {
+                if (markedCandidate == -1) {
+                    markedCandidate = i;
+                } else {
+                    return; // more than one candidate is marked on the ballot, so the ballot is invalid and not cast
+                }
+            }
+        }
+
+        if (markedCandidate != -1) {
+            voteCounts[markedCandidate]++; // increment the vote count for the marked candidate
+        }
+    }
+
+    /**
+     * Returns the configured ballot for the voting machine.
+     *
+     * @return the configured ballot for the voting machine
+     */
+    public Ballot getBallot() {
+        return configuredBallot.clone();
+    }
